a3d3c047beb203a773ccf1afbd10cc1823f27fb1
vaibhavpatil4345 <vaibhavpatil4345@gmail.com>
2026-03-14 23:23:34 +0530

    explored routing and pattern of routing

--- src/app.js
+++ src/app.js
@@ -2,10 +2,37 @@ const express=require('express')
 
 const app=express()
 
-app.use("/test",(req,resp)=>{
- resp.send('hello from node js')
+app.get("/user",(req,res)=>{
+    res.send('This is get call')
+})
+
+// app.post("/user",(req,res)=>{
+//     console.log(req.query)
+//     res.send('This is post call')
+// })
+
+app.post("/user/:userId",(req,res)=>{
+    console.log(req.params)
+    res.send('This is post call')
+})
+
+
+
+
+app.delete("/user",()=>{
+    res.send('This is delete call')
+})
+
+
+app.use("/test",(req,res)=>{
+ res.send('hello from node js')
+})
+
+app.use("/",(req,res)=>{
+    res.send('This is first route')
 })
 
 app.listen(3000,()=>{
     console.log('server has been listening on port 3000')
 })
+ 
