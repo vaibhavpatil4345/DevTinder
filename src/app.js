e1655e4259be134c39c7c714e2a81caaaef338a2
vaibhavpatil4345 <vaibhavpatil4345@gmail.com>
2026-03-21 13:57:09 +0530

    Data & API level sanitization

--- src/app.js
+++ src/app.js
@@ -1,83 +1,88 @@
 const express = require("express");
 const connectDB = require("./config/databse");
-const User=require("./models/user")
+const User = require("./models/user");
 
 const app = express();
 // middleware to convert json data to JS object
-app.use(express.json())
+app.use(express.json());
 
 // Dynamic Signup
-app.post("/signup",async (req,res)=>{
+app.post("/signup", async (req, res) => {
   // console.log(req.body)
-    // Create new instance of User model
-    const user= new User(req.body)
-    try {
-            await user.save()
-    res.send("User Added Successfully...")
-    }catch(err){
-        res.status(400).send("Error:",err.message)
-    }
-})
+  // Create new instance of User model
+  const user = new User(req.body);
+  try {
+    await user.save();
+    res.send("User Added Successfully...");
+  } catch (err) {
+    res.status(400).send("Error:", err.message);
+  }
+});
 
 // Get user by email
-app.get("/user", async(req,res)=>{
-  const userEmail=req.body.emailId
-  try{
-    const users= await User.find({emailId: userEmail})
-    if (users.length===0){
-      res.status(400).send("User not found...")
-    }else {
-        res.send(users)
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.emailId;
+  try {
+    const users = await User.find({ emailId: userEmail });
+    if (users.length === 0) {
+      res.status(400).send("User not found...");
+    } else {
+      res.send(users);
     }
-  }catch(err){
-    res.status(400).send("something went wrong....")
-  }  
-})
+  } catch (err) {
+    res.status(400).send("something went wrong....");
+  }
+});
 
 // Feed API, get/feed : Get all the users from API
-app.get("/feed",async (req,res)=>{
-  
-  try{
-    const users= await User.find({})
-    res.send(users)
-
-  }catch(err){
-    res.status(400).send("users not found....")
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (err) {
+    res.status(400).send("users not found....");
   }
-})
+});
 
 // DELETE API for user
-app.delete("/user",async(req,res)=>{
-  const userId=req.body.userId
-  try{
-      const user= await User.findByIdAndDelete(userId)
-      res.send("user deleted successfully....")
-  }catch(err){
-    res.status(400).send("user not found...")
+app.delete("/user", async (req, res) => {
+  const userId = req.body.userId;
+  try {
+    const user = await User.findByIdAndDelete(userId);
+    res.send("user deleted successfully....");
+  } catch (err) {
+    res.status(400).send("user not found...");
   }
-})
-
+});
 
 // Update api to update user info
-app.patch("/user",async (req,res)=>{
-const userId=req.body.userId
-// const emailId=req.body.userId
-const data=req.body
-  try{
-   const user= await User.findByIdAndUpdate({_id: userId}, data, {returnDocument : "after", runValidators:true})
-// update the user using emailId
-  //  const user= await User.findByIdAndUpdate(emailId, data, {returnDocument : "after"})
-   console.log("user",user)
-   res.send("user updated successfully...")
+app.patch("/user/:userId", async (req, res) => {
+  const userId = req.params.userId;
+  // const emailId=req.body.userId
+  const data = req.body;
+  try {
+    const ALLOWED_UPDATE = ["photoUrl", "gender", "age", "skills", "about"];
+    const isUpdatedAllowed = Object.keys(data).every((k) =>
+      ALLOWED_UPDATE.includes(k)
+    );
+    if (!isUpdatedAllowed) {
+      throw new Error("Updates not allowed");
+    }
 
-  }catch(err){
-    res.status(400).send("user not found")
+    if(data.skills.length > 10){
+      throw new Error("Skills should not be more than 10");
+    }
+    // update the user using emailId
+    //  const user= await User.findByIdAndUpdate(emailId, data, {returnDocument : "after"})
+     const user = await User.findByIdAndUpdate( userId , data, {
+      returnDocument: "after",
+      runValidators: true,
+    });
+    res.send("user updated successfully...");
+  } catch (err) {
+    res.status(400).send("user not found");
   }
-})
-
-
-
-
+});
 
 connectDB()
   .then(() => {
