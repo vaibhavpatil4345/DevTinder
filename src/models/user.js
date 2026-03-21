f0a5d045ab43a77c85d38eb730356021f8cf6b71
vaibhavpatil4345 <vaibhavpatil4345@gmail.com>
2026-03-21 14:16:31 +0530

    Email and password validations using npm validator library

--- src/models/user.js
+++ src/models/user.js
@@ -1,11 +1,22 @@
 const mongoose = require("mongoose");
+const validator= require("validator")
 
 // Create Schema here
 const userSchema = new mongoose.Schema({
   firstName: { type: String, required: true, minLength:5, maxLength:20 },
   lastName: { type: String },
-  emailId: { type: String, required: true, unique: true, lowercase:true, trim: true },
-  password: { type: String, required: true},
+  emailId: { type: String, required: true, unique: true, lowercase:true, trim: true, validate(value){
+    if (!validator.isEmail(value)){
+      throw new Error("invalid email address"+ value)
+    }
+  } 
+
+   },
+  password: { type: String, required: true,validate(value){
+    if (!validator.isStrongPassword(value)){
+      throw new Error("Password is not strong enough:"+ value)
+    }
+  } },
   age: { type: Number,min:18 },
   gender: { type: String, validate(value){
     if(!['male','female','other'].includes(value)){
