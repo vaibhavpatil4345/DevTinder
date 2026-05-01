f24681808773d6894c5a01cdae757d606251d165
vaibhavpatil4345 <vaibhavpatil4345@gmail.com>
2026-05-01 19:57:34 +0530

    updated db string

--- src/config/databse.js
+++ src/config/databse.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   await mongoose.connect(
-    "mongodb://vaibhavpatil4345:mYGNbn4IvV8mcR1O@ac-pab6rhu-shard-00-00.qr2dpgh.mongodb.net:27017,ac-pab6rhu-shard-00-01.qr2dpgh.mongodb.net:27017,ac-pab6rhu-shard-00-02.qr2dpgh.mongodb.net:27017/?ssl=true&replicaSet=atlas-robjwh-shard-0&authSource=admin&appName=NamasteNode"
+    "mongodb://vaibhavpatil4345:mYGNbn4IvV8mcR1O@ac-pab6rhu-shard-00-00.qr2dpgh.mongodb.net:27017,ac-pab6rhu-shard-00-01.qr2dpgh.mongodb.net:27017,ac-pab6rhu-shard-00-02.qr2dpgh.mongodb.net:27017/devTinder?ssl=true&replicaSet=atlas-robjwh-shard-0&authSource=admin&appName=NamasteNode"
   );
 };
 
-module.exports=connectDB
+module.exports = connectDB
